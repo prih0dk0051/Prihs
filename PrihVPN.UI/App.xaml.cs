6a71990f2270bd5ecbd8e882fefadc435d6cdc1c
prih0dk0051 <dima.firsov.2011@gmail.com>
2026-02-11 20:02:46 +0300

    Update App.xaml.cs

--- PrihVPN.UI/App.xaml.cs
+++ PrihVPN.UI/App.xaml.cs
@@ -2,24 +2,25 @@ using System.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using PrihVPN.UI.ViewModels;
 using PrihVPN.Infrastructure.Persistence;
+using PrihVPN.Infrastructure.Network;
 
 namespace PrihVPN.UI;
 
 public partial class App : Application
 {
-    public static IServiceProvider? Services { get; private set; }
-
     protected override void OnStartup(StartupEventArgs e)
     {
-        var serviceCollection = new ServiceCollection();
+        var services = new ServiceCollection();
         
-        // Регистрация слоев
-        serviceCollection.AddSingleton<LiteDbRepository>();
-        serviceCollection.AddSingleton<MainViewModel>();
+        // Регистрация инфраструктуры
+        services.AddSingleton<LiteDbRepository>();
+        services.AddSingleton<WindowsProxyController>();
+        
+        // Регистрация UI
+        services.AddSingleton<MainViewModel>();
 
-        Services = serviceCollection.BuildServiceProvider();
-
-        var mainWindow = new MainWindow { DataContext = Services.GetRequiredService<MainViewModel>() };
+        var provider = services.BuildServiceProvider();
+        var mainWindow = new MainWindow { DataContext = provider.GetRequiredService<MainViewModel>() };
         mainWindow.Show();
     }
 }
